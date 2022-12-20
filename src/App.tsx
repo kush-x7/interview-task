f99e4d4dbb020e5ac957ca925d59392e4b2521f1
kushagra <kushagratripathi7@gmail.com>
2022-12-20 22:49:34 +0530

    connected arrows

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const DraggableBox = ({ id }: any) => {
   const updateXarrow = useXarrow();
   return (
     <Draggable onDrag={updateXarrow} onStop={updateXarrow}>
-      <div className="card" id={id}>
+      <div className="card " id={id}>
         {id}
       </div>
     </Draggable>
@@ -37,9 +37,48 @@ function App() {
                 Match me with the options, I am 2
               </div>
               <DraggableBox id={"box-2"} />
-              <Xarrow start="box-1" end="box-2" />
+              <Xarrow
+                start="box-1"
+                end="box-2"
+                headColor={"#D3ECA7"}
+                lineColor={"#D3ECA7"}
+                tailColor={"#D3ECA7"}
+                strokeWidth={2}
+                headSize={4}
+                tailSize={4}
+                curveness={0.7}
+                // showHead={false}
+                headShape={"circle"}
+                showTail={true}
+                tailShape={"circle"}
+                passProps={{ cursor: "pointer", pointerEvents: "none" }}
+              />
             </Xwrapper>
+
             <Xwrapper>
+              <div className="card" id="box-3">
+                Match me with the options, I am 2
+              </div>
+              <DraggableBox id={"box-4"} />
+              <Xarrow
+                start="box-3"
+                end="box-4"
+                headColor={"#D3ECA7"}
+                lineColor={"#D3ECA7"}
+                tailColor={"#D3ECA7"}
+                strokeWidth={2}
+                headSize={4}
+                tailSize={4}
+                curveness={0.7}
+                // showHead={false}
+                headShape={"circle"}
+                showTail={true}
+                tailShape={"circle"}
+                passProps={{ cursor: "pointer", pointerEvents: "none" }}
+              />
+            </Xwrapper>
+
+            {/* <Xwrapper>
               <div className="card" id="box-3"></div>
               <DraggableBox id={"box-4"} />
               <Xarrow start="box-3" end="box-4" />
@@ -48,7 +87,7 @@ function App() {
               <div className="card" id="box-5"></div>
               <DraggableBox id={"box-6"} />
               <Xarrow start="box-5" end="box-6" />
-            </Xwrapper>
+            </Xwrapper> */}
           </div>
         </div>
       </main>
