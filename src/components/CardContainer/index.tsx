a372012ec53f586c1a938a30a837a1faa74fb658
kushagra <kushagratripathi7@gmail.com>
2022-12-21 09:03:57 +0530

    refactor

--- src/components/CardContainer/index.tsx
+++ src/components/CardContainer/index.tsx
@@ -1,8 +1,33 @@
 import "./cardContainer.css";
-import Xarrow, { useXarrow, Xwrapper } from "react-xarrows";
-import Draggable from "react-draggable";
 import CardRow from "./CardRow";
 import { useRef, useState } from "react";
+
+const cardData = [
+  {
+    key: 1,
+    leftCardText: "Match me with the options, I am 2",
+    leftCardPosition: "1",
+    rightCardText: "This is option 1 of MTF",
+    rightCardPosition: "2",
+    whichRowArrow: "first",
+  },
+  {
+    key: 2,
+    leftCardText: "Match me with the options, I am 1",
+    leftCardPosition: "3",
+    rightCardText: "This is option 2 of MTF",
+    rightCardPosition: "4",
+    whichRowArrow: "second",
+  },
+  {
+    key: 3,
+    leftCardText: "Match me with the options, I am 3",
+    leftCardPosition: "5",
+    rightCardText: "This is option 3 of MTF",
+    rightCardPosition: "6",
+    whichRowArrow: "third",
+  },
+];
 
 const CardContainer = () => {
   const [isArrowActive, setIsArrowActive] = useState(false);
@@ -28,34 +53,28 @@ const CardContainer = () => {
   };
 
   return (
-    <div className="cards-container">
-      <CardRow
-        leftCardPosition="1"
-        leftCardText="Match me with the options, I am 2"
-        rightCardPosition="2"
-        rightCardText="This is option 1 of MTF"
-        whichRowArrow="first"
-        handlePointerEnter={handlePointerEnter}
-        handlePointerLeave={handlePointerLeave}
-        handlePointerUp={handlePointerUp}
-        mergeArrow={mergeArrow}
-        isArrowActive={isArrowActive}
-        setIsArrowActive={setIsArrowActive}
-      />
-      <CardRow
-        leftCardPosition="3"
-        leftCardText="Match me with the options, I am 1"
-        rightCardPosition="4"
-        rightCardText="This is option 2 of MTF"
-        whichRowArrow="second"
-        handlePointerEnter={handlePointerEnter}
-        handlePointerLeave={handlePointerLeave}
-        handlePointerUp={handlePointerUp}
-        mergeArrow={mergeArrow}
-        isArrowActive={isArrowActive}
-        setIsArrowActive={setIsArrowActive}
-      />
-    </div>
+    <>
+      <div className="cards-container">
+        {cardData.map((data) => {
+          return (
+            <CardRow
+              key={data.key}
+              leftCardPosition={data.leftCardPosition}
+              leftCardText={data.leftCardText}
+              rightCardPosition={data.rightCardPosition}
+              rightCardText={data.rightCardText}
+              whichRowArrow={data.whichRowArrow}
+              handlePointerEnter={handlePointerEnter}
+              handlePointerLeave={handlePointerLeave}
+              handlePointerUp={handlePointerUp}
+              mergeArrow={mergeArrow}
+              isArrowActive={isArrowActive}
+              setIsArrowActive={setIsArrowActive}
+            />
+          );
+        })}
+      </div>
+    </>
   );
 };
 
